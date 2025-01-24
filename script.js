88968e3e46c29a7ea6fe7efaf0ae9130fd2676f3
Katamjeevana <93027334+Katamjeevana@users.noreply.github.com>
2025-01-24 16:19:00 +0530

    QRCode generation

---
+++ script.js
@@ -0,0 +1,30 @@
+let container = document.querySelector(".container");
+let qrInput = document.querySelector("#text")
+let genearateBtn = document.querySelector("#generate")
+let img = document.querySelector("#qr-img")
+
+let preInput
+
+
+genearateBtn.onclick = function () {
+  let input = qrInput.value.trim()
+
+  if (!input || input === preInput) {
+
+    alert("this fieldshould be empty and different withprevious input")
+    return
+
+  }
+
+  else {
+    preInput = input;
+    genearateBtn.innerText = "Generating QR Code....";
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${input}`;
+    img.onload = function () {
+      container.classList.add("active")
+      genearateBtn.innerText = "Generate QR Code"
+    }
+
+
+  }
+}
